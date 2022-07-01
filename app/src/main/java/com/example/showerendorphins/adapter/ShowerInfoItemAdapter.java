1952db207e0fba028f17553326d1077b0f426d12
박태영 <ty990520@naver.com>
2022-07-02 01:51:39 +0900

    #5 ui: Create listview & mypage ui

---
+++ app/src/main/java/com/example/showerendorphins/adapter/ShowerInfoItemAdapter.java
@@ -0,0 +1,59 @@
+package com.example.showerendorphins.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.showerendorphins.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShowerInfoItemAdapter extends ArrayAdapter implements AdapterView.OnItemClickListener {
+
+    private Context context;
+    private List list;
+
+    class ViewHolder {
+        public TextView itemDate;
+    }
+
+    public ShowerInfoItemAdapter(@NonNull Context context, ArrayList list) {
+        super(context, 0, list);
+        this.context = context;
+        this.list = list;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(context, "clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        final ViewHolder viewHolder;
+
+        if (convertView == null){
+            LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+            convertView = layoutInflater.inflate(R.layout.listview_item, parent, false);
+        }
+
+        viewHolder = new ViewHolder();
+        viewHolder.itemDate = (TextView) convertView.findViewById(R.id.item_date);
+
+        final String itemText = (String) list.get(position);
+        viewHolder.itemDate.setText(itemText);
+        viewHolder.itemDate.setTag(itemText);
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/showerendorphins/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/showerendorphins/ui/dashboard/DashboardFragment.java
@@ -4,29 +4,56 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
+import com.example.showerendorphins.R;
+import com.example.showerendorphins.adapter.ShowerInfoItemAdapter;
 import com.example.showerendorphins.databinding.FragmentDashboardBinding;
+
+import java.util.ArrayList;
 
 public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
+    private ListView showerInfoListView;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> dataSet =new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        View rootView = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        //===== 테스트를 위한 더미 데이터 생성 ===================
+        for (int i = 1; i<10; i++) {
+            dataSet.add("2022-0" + i+"-"+(i+10));
+        }
+        //========================================================
+
+        showerInfoListView = (ListView) rootView.findViewById(R.id.showerInfoListView_custom);
+        adapter= new ShowerInfoItemAdapter(getContext(),dataSet);
+        showerInfoListView.setAdapter(adapter);
+        showerInfoListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedItem = (String) view.findViewById(R.id.item_date).getTag().toString();
+                Toast.makeText(getContext(), selectedItem, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return rootView;
+    }
+
+    public void onListItemClick(ListView l, View v, int position, long id){
+        String strText = (String) l.getItemAtPosition(position);
+        Toast.makeText(this.getContext(), "클릭: " + position +" " + strText, Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -34,4 +61,16 @@ public class DashboardFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+}
+class ShowerInfo {
+    private String itemDate;
+
+    public ShowerInfo(String itemDate) {
+        this.itemDate = itemDate;
+    }
+
+    public String getItemDate() {
+        return itemDate;
+    }
+
 }
