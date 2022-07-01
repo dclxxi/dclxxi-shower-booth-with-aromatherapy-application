cb0efe85678d68dbee0cdffa4980cf28a7dba543
박태영 <ty990520@naver.com>
2022-07-01 17:00:04 +0900

    #5 ui: DrawerLayout 추가

--- app/src/main/java/com/example/showerendorphins/MainActivity.java
+++ app/src/main/java/com/example/showerendorphins/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.showerendorphins;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -10,6 +13,7 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.showerendorphins.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,9 +22,30 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        /*SIDE menu*/
+        final DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        /*BOTTOM TAB*/
+        NavigationView navigationView = findViewById(R.id.navigationView);
+
+
+        findViewById(R.id.imageMenu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // start에 지정된 Drawer 열기
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+        navigationView.setItemIconTintList(null);
+
+
+
+        /*main tab menu*/
+
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -29,8 +54,11 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+
     }
+
 
 }
