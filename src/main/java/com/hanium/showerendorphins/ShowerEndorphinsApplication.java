316f72181eed6b7a6637dd1b4c515caec2aada87
박태영 <ty990520@naver.com>
2022-07-15 15:13:23 +0900

    #7 feat: project initial setting

---
+++ src/main/java/com/hanium/showerendorphins/ShowerEndorphinsApplication.java
@@ -0,0 +1,13 @@
+package com.hanium.showerendorphins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowerEndorphinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowerEndorphinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanium/showerendorphins/ShowerEndorphinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanium.showerendorphins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowerEndorphinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
