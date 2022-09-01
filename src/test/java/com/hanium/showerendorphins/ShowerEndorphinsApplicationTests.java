4f0bb8bc12c1f46bb07c76c00be22de11508a2b7
taeyoung <ty009538@gmail.com>
2022-09-01 22:52:14 +0900

    chore: correct a typing error

--- src/test/java/com/hanium/showerendorphins/ShowerEndorphinsApplicationTests.java
+++ src/test/java/com/hanium/showerendorphins/ShowerEndorphinsApplicationTests.java
@@ -3,7 +3,7 @@ package com.hanium.showerendorphins;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ShowerEndorphinsApplicationTests {
 
     @Test
